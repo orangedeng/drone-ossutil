97b6aad09b346b3d50457c83a6fd64ef1d856ee8
Yuxing Deng <jxfa0043379@hotmail.com>
2022-09-16 11:27:28 +0800

    Add return code testing for 20x return

--- drone-gitee-release/create-release.sh
+++ drone-gitee-release/create-release.sh
@@ -1,6 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
+
+if [ -n "${DEBUG}" ]; then
+    set -x
+fi
 
 if [ -e /run/drone/env ]; then
     source /run/drone/env
@@ -18,5 +22,11 @@ DRONE_REPO=${DRONE_REPO}
 data="{\"access_token\":\"${GITEE_TOKEN}\",\"tag_name\":\"${DRONE_TAG}\",\"name\":\"${TITLE}\",\"body\":\"${NOTE}\",\"prerelease\":\"${PRERELEASE}\",\"target_commitish\":\"${DRONE_COMMIT}\"}"
 url="https://gitee.com/api/v5/repos/${DRONE_REPO}/releases"
 
-curl -v -X POST --header 'Content-Type: application/json;charset=UTF-8' $url -d "${data}"
+code=`curl --silent -X POST --header 'Content-Type: application/json;charset=UTF-8' $url -d "${data}" -o /dev/null -w '%{http_code}'`
 
+if ! [[ $code =~ 20* ]]; then
+    echo "failed to create release, return $code";
+    exit 1;
+fi
+
+exit 0;
